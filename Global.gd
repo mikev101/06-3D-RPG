de3156fa6a41d51296fa0a4a16e61a2e9d15f7e2
mikev101 <vealm@iu.edu>
2021-12-11 23:20:27 -0500

    Added Win screen and Main menu

--- Global.gd
+++ Global.gd
@@ -10,8 +10,14 @@ func _unhandled_input(_event):
 func update_score(s):
 	score += s
 	get_node("/root/Game/UI/Score").text = "Score: " + str(score)
+	if score == 40 and timer >= 0:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().change_scene("res://UI/Win.tscn")
+		Global.score = 0
+		Global.timer = 0
 	
 func update_time():
 	timer -= 1
 	get_node("/root/Game/UI/Time").text = "Timer: " + str(timer)
 	
+
